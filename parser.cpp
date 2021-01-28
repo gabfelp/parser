4b5d09651258b665a0b50ba6e7ae66ebe9b27383
Gabriel F <gabpaivapereira@gmail.com>
2021-01-28 10:01:25 -0500

    some adds, almost ending

--- parser.cpp
+++ parser.cpp
@@ -8,12 +8,41 @@ double Parser::elapsed ()
     return  tv.tv_sec + tv.tv_usec * 1e-6;
 }
 
+map<int,std::string> Parser::loadUrlMap(){
+  map<int,std::string> urlMap;
+  ifstream map(MAP_PATH);
+  if(map.fail()){
+    cout << "Url Map doesn't exist on " << MAP_PATH << endl;
+  }else{
+    // gets the website and numPages from argv
+    std::string line;
+    std::string delimiter = " ";
+    std::string word;
+    while(map){
+        std::getline(map, line);
+        int pos = 0;
+        if(line == ""){
+            continue;
+        }
+        
+        while((pos = line.find(delimiter)) != std::string::npos){
+            word = line.substr(0,pos);
+            line.erase(0,pos+delimiter.length());
+        }
+        urlMap.insert(pair<int,std::string>(stoi(line),word));
+        
+    }
+  }
+
+  return urlMap;
+}
+
 std::string Parser::cleanText(GumboNode* node) {
   if (node->type == GUMBO_NODE_TEXT) {
     return std::string(node->v.text.text);
   } else if (node->type == GUMBO_NODE_ELEMENT &&
-             node->v.element.tag != GUMBO_TAG_SCRIPT &&
-             node->v.element.tag != GUMBO_TAG_STYLE) {
+              node->v.element.tag != GUMBO_TAG_SCRIPT &&
+              node->v.element.tag != GUMBO_TAG_STYLE) {
     std::string contents = "";
     GumboVector* children = &node->v.element.children;
     for (unsigned int i = 0; i < children->length; ++i) {
@@ -45,7 +74,7 @@ std::vector<std::string> Parser::preProcessing(std::string content){
   // chars to remove
   char chars_to_remove[] = "\“\”|?#.,/!:";
   for(int j = 0; j < strlen(chars_to_remove);j++){
-      ret.erase(std::remove(ret.begin(), ret.end(), chars_to_remove[j]), ret.end());
+    ret.erase(std::remove(ret.begin(), ret.end(), chars_to_remove[j]), ret.end());
   }
   /*
   //for replacing m0 with m²
@@ -70,12 +99,67 @@ std::vector<std::string> Parser::preProcessing(std::string content){
 
 }
 
-void Parser::feedMap(std::vector<std::string> doc){
-  //add words in map
+void Parser::feedMap(int docNumber,std::vector<std::string> doc){
+  int wordCount = 0;
   for(string re : doc){
-    cout<<re<<endl;
+    if ( tokenMap.find(re) == tokenMap.end() ) {
+      // word not found, add word + (doc+occurrence)
+      map<int,vector<int>> sub;
+      sub.insert(pair<int,vector<int>>(docNumber,{wordCount}));
+
+      tokenMap.insert(pair<std::string,map<int,vector<int>>>(re,sub));
+
+    } else {
+      // word found, check doc
+      
+      //check if doc already exists
+      if ( tokenMap[re].find(docNumber) == tokenMap[re].end() ) {
+        // doc not found, add doc+occurrence
+        tokenMap[re].insert(pair<int,vector<int>>(docNumber,{wordCount}));
+      } else {
+        // doc found, add occurrence
+        tokenMap[re][docNumber].push_back(wordCount);
+
+      }
+
+
+    }
+    //cout<<re<<endl;
+    wordCount++;
+  }
+  TOTAL_WORDS_COUNT+= wordCount;
+}
+
+void Parser::printVocabulary(){
+  for(auto iter = tokenMap.begin(); iter != tokenMap.end(); iter++){
+    cout << iter->first << " ";
+    for(auto iter2 = iter->second.begin(); iter2 != iter->second.end(); iter2++){
+      cout << iter2->first << " { ";
+      for(auto elem : iter2->second){
+        cout << elem << " ";
+      }
+      cout << "}" << endl;
+    }
+  }
+}
+
+void Parser::printOutput(){
+  int size = 0;
+  size += tokenMap.size() * sizeof(string); // number of words
+  
+  
+  for(auto iter = tokenMap.begin(); iter != tokenMap.end(); iter++){
+    size +=iter->second.size() * sizeof(int);
+    for(auto iter2 = iter->second.begin(); iter2 != iter->second.end(); iter2++){
+      size +=iter2->second.size() * sizeof(int);
+    }
   }
 
+  //for Kb
+  float size_kb = size / 1000.0; 
+
+  cout << "Inverted Index Size (in Kbytes): " << size_kb << endl;
+  
 }
 
 bool Parser::saveIndex(){
@@ -91,12 +175,19 @@ bool Parser::saveIndex(){
     return true;
 }
 
-void Parser::start(map<int,std::string> urlMap){
+void Parser::start(){
     double t0 = elapsed();
 
     for(int i = 0; i < PAGES_TO_PARSE; i ++){
       std::string contents;
-      ifstream file(COLLECTION_PATH+to_string(i)+".html");
+      ifstream file(COLLECTION_PATH + to_string(i)+".html");
+
+      if(file.fail()){
+        cout << "File" << COLLECTION_PATH+to_string(i)<<".html not found, stopping."<<endl;
+        PAGES_TO_PARSE = i;
+        break;
+      }
+
       file.seekg(0,std::ios::end);
       contents.resize(file.tellg());
       file.seekg(0, std::ios::beg);
@@ -109,59 +200,40 @@ void Parser::start(map<int,std::string> urlMap){
 
       std::vector<std::string> preProcessedDoc = preProcessing(cleanWebsite);
       
-      feedMap(preProcessedDoc);
+      feedMap(i, preProcessedDoc);
     }
-
-    
-    
 
 
     double t1 = elapsed();
 
     TOTAL_TIME = t1-t0;
 
-    std::cout << "Average Parsing time per page : "<< TOTAL_TIME/PAGES_TO_PARSE << "s\n";
-    std::cout << "Total time : "<< TOTAL_TIME << "s\n";
+
+    std::cout << "Total Words Count : "  << TOTAL_WORDS_COUNT << endl;
+    std::cout << "Number of Terms in Vocabulary : " << tokenMap.size() << endl;
+    printOutput();
+
+    std::cout << "Average Parsing time per page : "<< TOTAL_TIME/PAGES_TO_PARSE << "s" << endl;
+    std::cout << "Total time : "<< TOTAL_TIME << "s" << endl;
 }
 
 
 int main(int argc, char **argv){
     
-    map<int,std::string> urlMap;
+    
     Parser p;
 
+    
+    /* 
+    map<int,std::string> urlMap = p.loadUrlMap();
 
-    ifstream map(MAP_PATH);
-    if(map.fail()){
-      cout << "Url Map doesn't exist, continuing anyway" << endl;
-    }else{
-      // gets the website and numPages from argv
-      std::string line;
-      std::string delimiter = " ";
-      std::string word;
-      while(map){
-          std::getline(map, line);
-          int pos = 0;
-          if(line == ""){
-              continue;
-          }
-          
-          while((pos = line.find(delimiter)) != std::string::npos){
-              word = line.substr(0,pos);
-              line.erase(0,pos+delimiter.length());
-          }
-          urlMap.insert(pair<int,std::string>(stoi(line),word));
-          
-      }
+    for(auto const& elem : urlMap){
+        cout << elem.first << " : " << elem.second << endl;
     }
-    /*
-    for(auto const& elem : urlmap){
-        cout << elem.first << ":" << elem.second << endl;
-    }
+    cout<<urlMap.size();
     */
-   cout<<urlMap.size();
 
-    //p.start(urlMap);
+    p.start();
 
     return 0;
 }
--- parser.h
+++ parser.h
@@ -26,9 +26,12 @@ std::string VOCAB_PATH = "./out/vocabulary.txt"; // place and name of map
 
 double TOTAL_TIME = 0;
 const int MAX_LENGTH = 100;
-int PAGES_TO_PARSE = 1;
+int PAGES_TO_PARSE = 10;
+int TOTAL_WORDS_COUNT = 0;
 
-map<std::string,vector<pair<int,int>>> tokenMap;
+// the key of first map is the word.
+// the second map represents a document and all occurrences positions in the document 
+map<std::string,map<int,vector<int>>> tokenMap;
 
 
 class Parser
@@ -37,8 +40,13 @@ class Parser
         // return the time
         static double elapsed ();
 
+        /* loads the url map from collection.
+        each line of the file contains a url followed by a number. 
+        The number is the name of the .html file for that url on the collection, for example "2.html"*/
+        map<int,std::string> loadUrlMap();
+
         // runs the parsing
-        void start(map<int,std::string> urlMap);
+        void start();
 
         // do the parsing on the html
         static std::string cleanText(GumboNode* node);
@@ -46,8 +54,14 @@ class Parser
         // pre process before token creation
         static std::vector<std::string> preProcessing(std::string content);
 
-        // feed the tokenMap
-        void feedMap(std::vector<std::string> doc);
+        // add words in the vocabulary
+        void feedMap(int docNumber, std::vector<std::string> doc);
+
+        // print the vocabulary
+        static void printVocabulary();
+
+        // print the output results
+        static void printOutput();
 
         // save Vocabulary
         bool saveIndex();
